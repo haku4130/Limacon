ccf40af502d0fc2d4f6f5904ade3928294d8a3b7
haku4130 <101144756+haku4130@users.noreply.github.com>
2022-09-29 19:24:22 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -90,3 +90,57 @@ int comp (double a, double b) {
     if (a > b) return 1;
     return 2;
 }
+
+Curve::Curve(double a, double b){
+    koef_a = a;
+    koef_b = b;
+}
+
+void Curve::set_koefs(double a, double b){
+    koef_a = a;
+    koef_b = b;
+}
+
+void Curve::get_polar_equation() const{
+    cout << "r = " << koef_b << " + " << koef_a << " * cos(theta)\n";
+}
+
+double Curve::radius(double theta) const {
+    return (koef_b + koef_a * cos(theta));
+}
+
+void Curve::get_type() const{
+    if (koef_a == 0){
+        cout << "Circle.\n";
+        return;
+    }
+    if (koef_a == 2 * koef_b){
+        cout << "Trisectrix.\n";
+        return;
+    }
+    int i = comp(koef_a, koef_b);
+    if (i == 0) cout << "Cardioid.\n";
+    else if (i == 1) cout << "With loop.\n";
+    else cout << "Oval.\n";
+}
+
+double Curve::get_area() const{
+    if (koef_b < koef_a) return (pow(koef_b, 2) + (pow(koef_a, 2) / 2)) * (acos(-koef_b / koef_a) + acos(koef_b / koef_a));
+    return (pow(koef_b, 2) + (pow(koef_a, 2) / 2)) * M_PI;
+}
+
+unsigned long long Curve::special_number() const {
+    string s1 = to_string(koef_a);
+    string s2 = to_string(pow(koef_b, 2));
+    return s1.length() + s2.length();
+}
+
+void Curve::get_cartesian_equation(char *res_char) const {
+    string s1 = "(x^2 + y^2 - ";
+    string s2 = to_string(koef_a);
+    string s3 = " * x)^2 = ";
+    string s4 = to_string(pow(koef_b, 2));
+    string s5 = " * (x^2 + y^2)";
+    string res = s1 + s2 + s3 + s4 + s5;
+    strcpy(res_char, res.c_str());
+}
